cb26c1191e96602035f883290658203dbb08456c
Viktor Sukochev <sukochevvv@gmail.com>
2021-06-16 07:04:34 +0300

    Initial project setup

---
+++ Sources/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ProcessesManager
+//
+//  Created by Victor Sukochev on 16.06.2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Sources/UI/Main/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ProcessesManager
+//
+//  Created by Victor Sukochev on 16.06.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
